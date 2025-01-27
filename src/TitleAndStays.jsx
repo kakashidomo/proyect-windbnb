78d85c0373fc8687243fda4de959a57f0993ac35
Gustavo Lastra <lastraemiliano1@gmail.com>
2025-01-27 01:20:33 -0600

    se creo nuevo componente y se movio contenido del componente App

---
+++ src/TitleAndStays.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function TitleAndStays() {
+  return (
+    <>
+
+    <div className='flex items-center justify-between mx-2'>
+                <h1 className='font-bold text-2xl text-gray-900'>
+                    Stays in Finland
+                </h1>
+                <h2 className='font-medium text-sm text-gray-500'>
+                    12+ Stays
+                </h2>
+            </div>
+    </>
+  )
+}
